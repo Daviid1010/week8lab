d46e924d27262464bd800efe791a3c3728edda0d
David Sheehy <x16440304@student.ncirl.ie>
2016-11-10 13:36:05 +0000

    excercise 3

--- index.js
+++ index.js
@@ -13,3 +13,10 @@ console.log("Good joke? " + myBoolean);
 //Exercise 2
 
 console.log('Pepperoni!', 'bacon!')
+
+//Exercise 3
+
+console.log(17 + 3.5)
+console.log(2016-1969)
+console.log(65/240)
+console.log(0.2708333333333333 * 100)
